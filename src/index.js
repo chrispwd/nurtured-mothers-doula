dd506dcb6329f9154f1c700a5ccc747d7f812743
chrispwd <chrispwd@protonmail.com>
2023-09-19 00:15:37 -0600

    finish navbar submenu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const clickHandle = (event) => {
-  alert("Welcome to the site!");
+const toggleSubmenuVisibility = (dropdownArrow) => {
+    const submenu = document.getElementById("postpartum-dropdown");
+    const arrow = document.getElementById(dropdownArrow.id);
+    if (submenu.style.display === "block") {
+        submenu.style.display = "none";
+        arrow.innerHTML = " ⏷";
+    } else {
+        submenu.style.display = "block";
+        arrow.innerHTML = " ⏶";
+    }
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+// TODO :: Configure primary / secondary / tertiary colors
+// TODO :: Choose a prose font
 module.exports = {
     content: [
         "./src/**/*.{html,js}",
@@ -7,6 +10,25 @@ module.exports = {
     theme: {
         fontFamily: {
             'header': ["Fraunces", "serif"],
+        },
+        colors: {
+            'foreground': {
+                'light': '#C9CDCF',
+                DEFAULT: '#1d1f21',
+                'dark': '#0A0A0B',
+            },
+            'background': {
+                'light': '#FAF6F0',
+                DEFAULT: '#F8E9E2',
+                'dark': '#e6d2b2',
+            },
+            'pink': '#ff49db',
+            'orange': '#ff7849',
+            'green': '#13ce66',
+            'yellow': '#ffc82c',
+            'gray-dark': '#273444',
+            'gray': '#8492a6',
+            'gray-light': '#d3dce6',
         },
         extend: {},
     },

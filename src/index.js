0391472664e4d6a6d2e1e6e892e878c19d77493b
chrispwd <chrispwd@protonmail.com>
2023-11-03 00:51:23 -0600

    fix header links, fix postpartum dropdown, update banners

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const toggleSubmenuVisibility = (dropdownArrow) => {
     const arrow = document.getElementById(dropdownArrow.id);
     if (submenu.style.display === "block") {
         submenu.style.display = "none";
-        arrow.innerHTML = " ⏷";
+        arrow.innerHTML = `<svg class="inline text-foreground-default absolute top-0 left-0" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
+              </svg>`;
     } else {
         submenu.style.display = "block";
-        arrow.innerHTML = " ⏶";
+        arrow.innerHTML = `<svg class="inline text-foreground-default absolute top-0 left-0" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/></svg>`;
     }
 }
 
